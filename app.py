0e1ba013ef64ed65f74a1e579513a109e3661337
HardPlant <abc7988se@naver.com>
2017-09-25 22:44:12 +0900

    logout

--- app.py
+++ app.py
@@ -65,6 +65,12 @@ def login():
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
+@app.route('/logout')
+def logout():
+    session.pop('logged in', None) # doesn't check if user logged in
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 if __name__ == '__main__':
     app.run()
 
