31567be8e16eb6504d44b06e2cd0a7905db37b0e
HardPlant <abc7988se@naver.com>
2017-09-25 22:43:10 +0900

    login

--- app.py
+++ app.py
@@ -51,7 +51,19 @@ def add_entry():
     flash('New entry was succesfully posted')
     return redirect(url_for('show_entries'))
 
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invaid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invaid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run()
