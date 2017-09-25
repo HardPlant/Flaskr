6e79111bdd1c3a24c65f45765781e324f2a92093
HardPlant <abc7988se@naver.com>
2017-09-25 22:40:34 +0900

    add entry, checks session

--- app.py
+++ app.py
@@ -41,6 +41,17 @@ def show_entries():
     entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was succesfully posted')
+    return redirect(url_for('show_entries'))
+
+
 
 if __name__ == '__main__':
     app.run()
