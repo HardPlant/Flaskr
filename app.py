107dd8b10c52ddd56f9867f739ed566228ffbce6
HardPlant <abc7988se@naver.com>
2017-09-25 22:38:39 +0900

    show_entries() searches db, return dict

--- app.py
+++ app.py
@@ -35,6 +35,13 @@ def before_request():
 def teardown_request(exception):
     g.db.close()
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
 if __name__ == '__main__':
     app.run()
 
