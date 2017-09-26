ffa20a726d8336dbb739db611b3b0dc1b5b7ec1a
HardPlant <abc7988se@naver.com>
2017-09-26 13:01:38 +0900

    Login/out Function

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -21,5 +21,14 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert 'No entries here so far' in rv.data
 
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirect=True)
+
 if __name__ == '__main__':
     unittest.main()
