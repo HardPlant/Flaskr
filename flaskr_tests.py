c838e90398c0b4636a5e96990007745e68dfd693
HardPlant <abc7988se@naver.com>
2017-09-26 12:56:12 +0900

    init test database

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -1,12 +1,22 @@
+import os
+import tempfile
 import unittest
+
+import flaskr
 
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
     def tearDown(self):
-        pass
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
 
 
 if __name__ == '__main__':
+    unittest.main()
