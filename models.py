64abb082858f224ff60c5a8ac4e6d485f0a6b93f
HardPlant <abc7988se@naver.com>
2017-09-27 21:58:18 +0900

    fixed mistype

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primart_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
 
